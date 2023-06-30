b143ac3baaba51e309405fbf59ba5b28a7ebf061
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2023-06-30 11:26:41 +0200

    Recent experiments

--- diffusion.py
+++ diffusion.py
@@ -32,9 +32,9 @@ class diffusionRecipe(A.recipe):
         self.the_props.set_ion("s", 1, 0, 0, self.diffusivity)
         self.the_props.set_ion("p", 1, 0, 0, self.diffusivity)
 
-        self.ch_0 = 420
-        self.ch_1 = 1000
-        self.ch_2 = 500
+        self.ch_0 = 2
+        self.ch_1 = 10
+        self.ch_2 = -5
         self.times_0 = [0.75]
         self.times_1 = [0.5, 1.5,]
         self.times_2 = [1.0]
@@ -57,19 +57,22 @@ class diffusionRecipe(A.recipe):
         tag_dendrite1 = 2
         tag_dendrite2 = 3
 
+        #     /
+        #    /
+        # --*
+        #    \
+        #     \
         sm = tree.append(A.mnpos,
-                         A.mpoint(0, -self.length_soma/2, 0, self.radius_soma),
-                         A.mpoint(0,  self.length_soma/2, 0, self.radius_soma),
+                         (0, 0,                0, self.radius_soma),
+                         (0, self.length_soma, 0, self.radius_soma),
                          tag=tag_soma)
-
         _ = tree.append(sm,
-                        A.mpoint(0, self.length_soma/2, 0, self.radius_dendrite),
-                        A.mpoint(0, self.length_dendrite+self.length_soma/2, 0, self.radius_dendrite),
+                        (0, 0,                    0, self.radius_dendrite),
+                        (0, self.length_dendrite, 0, self.radius_dendrite),
                         tag=tag_dendrite1)
-
         _ = tree.append(sm,
-                        A.mpoint(0, self.length_soma/2, 0, self.radius_dendrite),
-                        A.mpoint(0, self.length_dendrite + self.length_soma/2, 0, self.radius_dendrite),
+                        (0, 0,                    0, self.radius_dendrite),
+                        (0, self.length_dendrite, 0, self.radius_dendrite),
                         tag=tag_dendrite2)
 
         labels = A.label_dict({"soma_region": f"(tag {tag_soma})",
@@ -99,7 +102,6 @@ class diffusionRecipe(A.recipe):
                  .discretization(A.cv_policy(f'(max-extent {self.delta_x})'))
                  # TODO: This shouldn't be needed, but is
                  .set_ion("s", int_con=0.0, diff=self.diffusivity)
-                 .set_ion("p", int_con=0.0, diff=self.diffusivity)
                  .place('"dendrite1_synapses"', A.synapse("synapse_with_diffusion"), "syn_1")
                  .place('"dendrite2_synapses"', A.synapse("synapse_with_diffusion"), "syn_2")
                  .place('"soma"', A.synapse("synapse_with_diffusion"), "syn_0")
@@ -113,34 +115,34 @@ class diffusionRecipe(A.recipe):
         return self.the_props
 
     def event_generators(self, _):
-        return [A.event_generator("syn_0",  self.ch_0, A.explicit_schedule(self.times_0)),
-            A.event_generator("syn_1",  self.ch_1, A.explicit_schedule(self.times_1)),
-        A.event_generator("syn_2", -self.ch_2, A.explicit_schedule(self.times_2))]
+        return [A.event_generator("syn_0", self.ch_0, A.explicit_schedule(self.times_0)),
+                A.event_generator("syn_1", self.ch_1, A.explicit_schedule(self.times_1)),
+                A.event_generator("syn_2", self.ch_2, A.explicit_schedule(self.times_2))]
 
     def probes(self, _):
         return [A.cable_probe_ion_diff_concentration('"soma"', "s"),
                 A.cable_probe_ion_diff_concentration('"dendrite1_synapses"', "s"),
-                A.cable_probe_ion_diff_concentration('"dendrite2_synapses"', "s"),
+                A.cable_probe_ion_diff_concentration('"dendrite2_synapses"', "s"), # 2
 
                 # SV ASSIGNED
                 A.cable_probe_density_state('"soma"', "neuron_with_diffusion", "sV"),
                 A.cable_probe_point_state(0, "synapse_with_diffusion", "sV"),
-                A.cable_probe_point_state(1, "synapse_with_diffusion", "sV"),
+                A.cable_probe_point_state(1, "synapse_with_diffusion", "sV"), # 5
 
                 # sV STATE
                 A.cable_probe_density_state('"soma"', "neuron_with_diffusion", "sVs"),
                 A.cable_probe_point_state(0, "synapse_with_diffusion", "sVs"),
-                A.cable_probe_point_state(1, "synapse_with_diffusion", "sVs"),
+                A.cable_probe_point_state(1, "synapse_with_diffusion", "sVs"), # 8
 
                 # ???
                 A.cable_probe_density_state_cell("neuron_with_diffusion", "sV"),
                 A.cable_probe_ion_diff_concentration_cell("s"),
-                A.cable_probe_point_state(2, "synapse_with_diffusion", "sV"),
+                A.cable_probe_point_state(2, "synapse_with_diffusion", "sV"), # 11
 
                 # Probe DENSITY state at different places
                 A.cable_probe_density_state('"soma"', "neuron_with_diffusion", "sV"),
                 A.cable_probe_density_state('"dendrite1_synapses"', "neuron_with_diffusion", "sV"),
-                A.cable_probe_density_state('"dendrite2_synapses"', "neuron_with_diffusion", "sV"),
+                A.cable_probe_density_state('"dendrite2_synapses"', "neuron_with_diffusion", "sV"), # 14
         ]
 
 def run_sim(points, r_soma, r_dend, tx):
@@ -150,7 +152,7 @@ def run_sim(points, r_soma, r_dend, tx):
     rec = diffusionRecipe(points, r_soma, r_dend)
     sim = A.simulation(rec)
 
-    hdls = [sim.sample((0, i), A.regular_schedule(dt_)) for i,_ in enumerate(rec.probes(0))]
+    hdls = [sim.sample((0, i), A.regular_schedule(0.1*dt_)) for i,_ in enumerate(rec.probes(0))]
 
     sim.run(dt=dt_, tfinal=t_final)
     fg, axs = plt.subplots(2, 3, sharex=True, )
@@ -173,12 +175,19 @@ def run_sim(points, r_soma, r_dend, tx):
             ax.plot(data[:, 0], data[:, 1], ls=':')
 
     ax = axs[1][2]
-    for data, meta in sim.samples(hdls[12]):
+    for data, meta in sim.samples(hdls[14]):
             ax.plot(data[:, 0], data[:, 1], ls=':')
 
+    for data, meta in sim.samples(hdls[11]):
+            print(meta)
     for data, meta in sim.samples(hdls[7]):
             print(meta)
     for data, meta in sim.samples(hdls[8]):
+            print(meta)
+
+    for data, meta in sim.samples(hdls[12]):
+            print(meta)
+    for data, meta in sim.samples(hdls[13]):
             print(meta)
 
     axs[0][0].set_ylabel('Xd $(mol/l)$')
@@ -191,19 +200,34 @@ def run_sim(points, r_soma, r_dend, tx):
         axs[0][ix].set_title(title)
         axs[-1][ix].set_xlabel('Time $(t/ms)$')
 
+    # total injected
+    N = rec.ch_0*len(rec.times_0) + rec.ch_1*len(rec.times_1) + rec.ch_2*len(rec.times_2)
+    # equilibrium concentration
+    X = N/rec.volume
+    # per CV count = concentration * CV_volume
+    n_soma = X*rec.delta_x*np.pi*rec.radius_soma**2
+    n_dend = X*rec.delta_x*np.pi*rec.radius_dendrite**2
+    axs[0][0].hlines(X, 0, 5, color='0.8')
+    axs[0][1].hlines(X, 0, 5, color='0.8')
+    axs[0][2].hlines(X, 0, 5, color='0.8')
+    axs[1][0].hlines(n_soma, 0, 5, color='0.8')
+    axs[1][1].hlines(n_dend, 0, 5, color='0.8')
+    axs[1][2].hlines(n_dend, 0, 5, color='0.8')
+
     fg.savefig(f'diff-dx={rec.delta_x}-rs={rec.radius_soma}-rd={rec.radius_dendrite}.png')
 
+    total = None
     for data, meta in sim.samples(hdls[9]):
         total = np.zeros_like(data[:, 0])
         for ix, _ in enumerate(meta):
             total += data[:, ix + 1]
             tx.plot(data[:, 0], total, label=f"Total particles dx={rec.delta_x} rs={rec.radius_soma} rd={rec.radius_dendrite}")
-        print(f"Equilibrium")
-        print(f" * Particles      | {np.max(total):10.4f}")
-    for data, meta in sim.samples(hdls[10]):
-            print(f" * Concentration  | {data[-1, 1]:10.4f}")
-            print(f" * Particles'     | {data[-1, 1]*rec.volume:10.4f}")
 
+    print(f"               | Total      |")
+    print(f"Particles      | {np.max(total):10.4f} |")
+    for data, meta in sim.samples(hdls[10]):
+            print(f"Concentration  | {data[-1, 1]:10.4f}")
+            print(f"Particles'     | {data[-1, 1]*rec.volume:10.4f}")
 
 fg, ax = plt.subplots()
 for dx in [1, 2]:
